23843c43d3f6c5b4487cf61ceaf2ade113fa2dc2
Usuario <Usuario@GIGABYTE.home>
2016-03-08 23:41:51 +0100

    Creada clase RecibirDaño inutilizada por ahora

---
+++ src/Batalla/RecibirDaño.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Batalla;
+
+import trabajoprog.Personaje.Asesino;
+
+/**
+ *
+ * @author Usuario
+ */
+public class RecibirDaño {
+    Asesino jug2=new Asesino();
+    
+   
+}
